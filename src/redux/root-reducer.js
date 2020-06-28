a829369efc55ea75f7876b145ed7dd78262a3da1
Fernanda Alves <fernanda__m@hotmail.com>
2020-06-27 23:05:07 -0300

    implemented saving to local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from "redux";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
 import listReducer from "./list/list.reducer";
 import cardReducer from "./card/card.reducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["lists"],
+};
+
 const rootReducer = combineReducers({
   lists: listReducer,
   cards: cardReducer,
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
